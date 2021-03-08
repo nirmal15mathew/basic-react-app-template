a94534f8257981e69bfb0873a49b77b93da605df
Tj <55346160+nirmal15mathew@users.noreply.github.com>
2021-03-08 19:49:35 +0530

    ADDED BASIC STYLE AND STRUCTURE

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,38 +1,37 @@
-import { useRef } from "react"
+import { useRef } from "react";
+import "./LoginForm.css"
 
 function LoginForm(props) {
-    // using ref for handling inputs
-    const username  = useRef(null)
-    const password = useRef(null)
+  // using ref for handling inputs
+  const username = useRef(null);
+  const password = useRef(null);
 
-    // the function for setting the credentials according to input
-    const { setCreds } = props
-    const submitCreds = (e) => {
-        // for preventing the page from refreshing
-        e.preventDefault()
-        let creds = {
-            username: username.current.value,
-            password: password.current.value
-        }
-        setCreds(creds) // send credentials to the parent component
-    }
-    return (
-        <form>
-            <fieldset>
-                <legend>
-                    Username
-                </legend>
-                <input type="text" required ref={username}></input>
-            </fieldset>
-            <fieldset>
-                <legend>
-                    Password
-                </legend>
-                <input type="password" required ref={password}></input>
-            </fieldset>
-            <button onClick={e => submitCreds(e)}>Login</button>
-        </form>
-    )
+  // the function for setting the credentials according to input
+  const { setCreds } = props;
+  const submitCreds = (e) => {
+    // for preventing the page from refreshing
+    e.preventDefault();
+    let creds = {
+      username: username.current.value,
+      password: password.current.value,
+    };
+    setCreds(creds); // send credentials to the parent component
+  };
+  return (
+    <form className="login-form">
+      <fieldset className="field username">
+        <legend className="field-title username">Username</legend>
+        <input type="text" required ref={username}></input>
+      </fieldset>
+      <fieldset className="field password">
+        <legend className="field-title password">Password</legend>
+        <input type="password" required ref={password}></input>
+      </fieldset>
+      <button onClick={(e) => submitCreds(e)} className="button submit-button">
+        Login
+      </button>
+    </form>
+  );
 }
 
 export default LoginForm;
--- src/components/SignedIn.jsx
+++ src/components/SignedIn.jsx
@@ -1,36 +1,36 @@
 import { Route, Switch, Link } from "react-router-dom";
-import AppContainer from "./AppContainer"
-import AppHeader from "./AppHeader"
+import AppContainer from "./AppContainer";
+import AppHeader from "./AppHeader";
 
 // pages for routing
-import Home from "../pages/Home"
-import Dashboard from "../pages/Dashboard"
-import Profile from "../pages/Profile"
+import Home from "../pages/Home";
+import Dashboard from "../pages/Dashboard";
+import Profile from "../pages/Profile";
 
-function SignedIn(props){
-    // for signing out and to get the current logged in user
-    const {user, setLogin} = props
-    return (
-        <AppContainer>
-            <AppHeader>
-                <Link to="/">Home</Link>
-                <Link to="/dashboard">Dashboard</Link>
-                <Link to="/profile">Profile</Link>
-            </AppHeader>
-            <Switch>
-                <Route path="/" exact>
-                    <Home></Home>
-                </Route>
-                <Route path="/dashboard" component={Dashboard}/>
-                <Route path="/profile">
-                    <Profile>
-                        <h1>Signed In {user}</h1>
-                        <button onClick={_ => setLogin(false)}>Logout</button>
-                    </Profile>
-                </Route>
-            </Switch>
-        </AppContainer>
-    )
+function SignedIn(props) {
+  // for signing out and to get the current logged in user
+  const { user, setLogin } = props;
+  return (
+    <AppContainer>
+      <AppHeader>
+        <Link to="/">Home</Link>
+        <Link to="/dashboard">Dashboard</Link>
+        <Link to="/profile">Profile</Link>
+      </AppHeader>
+      <Switch>
+        <Route path="/" exact>
+          <Home></Home>
+        </Route>
+        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/profile">
+          <Profile>
+            <h1>Signed In {user}</h1>
+            <button onClick={(_) => setLogin(false)}>Logout</button>
+          </Profile>
+        </Route>
+      </Switch>
+    </AppContainer>
+  );
 }
 
 export default SignedIn;
